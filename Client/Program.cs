f185c79cee7cc03447b4c1b1f08738c420579fdd
mauvefalcone <168620095+mauvefalcone@users.noreply.github.com>
2025-03-16 19:53:59 +0100

    initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        string serverIp = "127.0.0.1";
+        int port = 12345;
+
+        TcpClient client = new TcpClient();
+        client.Connect(serverIp, port);
+        Console.WriteLine("Connected to server.");
+
+        NetworkStream stream = client.GetStream();
+
+        byte[] message = Encoding.ASCII.GetBytes("Hello from Bruce!");
+        stream.Write(message, 0, message.Length);
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("Recieved from server: " + response);
+
+        client.Close();
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+class Server
+{
+    static void Main()
+    {
+        int port = 12345;
+        TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+
+        server.Start();
+        Console.WriteLine($"Server started on 127.0.0.1:{port}");
+
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Client connected.");
+
+        NetworkStream stream = client.GetStream();
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+        string message = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+        Console.WriteLine("Received from client: " + message);
+
+        byte[] response = Encoding.ASCII.GetBytes("Hello Sir from Alfred!");
+        stream.Write(response, 0,  response.Length);
+
+        client.Close();
+        server.Stop();
+    }
+}
